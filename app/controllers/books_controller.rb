ed0ba310bd4ef765231509d293bf453a2399fd7e
EC2 Default User <ec2-user@ip-172-31-1-76.ap-northeast-1.compute.internal>
2023-06-15 13:08:54 +0000

    ver.0.9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,14 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      flash[:alert] = "2 errors prohibited this book from being saved:"
+      @book = Book.all
+      render :index
+    end
   end
 
   def show
